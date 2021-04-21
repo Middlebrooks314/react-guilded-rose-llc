f0f6ea0bef5c7ee2186433ebbebed8d92cabaf83
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-04-21 11:36:52 -0500

    Add eslint.

--- _tests_/dummy.test.js
+++ _tests_/dummy.test.js
@@ -1,5 +1,5 @@
-describe("Dummy function", () => {
-  test("it should be a passing test", () => {
+describe('Dummy function', () => {
+  test('it should be a passing test', () => {
     expect(true).toEqual(true)
-  });
-});
+  })
+})
