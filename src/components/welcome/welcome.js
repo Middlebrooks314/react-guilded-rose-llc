c1f6060a79c7eed934cc9888cd0bc92648583dde
Malisa Middlebrooks <middlebrooks314@gmail.com>
2021-06-24 21:41:56 -0500

    Add welcome string styles

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -3,7 +3,7 @@ import React from "react"
 const Welcome = (props) => {
   const { welcomeString } = props
   return (
-    <h1 data-testid="welcome-message">{welcomeString}</h1>
+    <h1 data-testid="welcome-message" id="welcome-message">{welcomeString}</h1>
   )
 }
 
